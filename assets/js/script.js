ee8067fc9c5a428e1b18ff2178dbbd36e6b2d4dc
Pedro Díaz Rivera <pedro.neox@gmail.com>
2024-07-15 12:32:59 -0400

    Se ingresan nuevas funciones visuales al archivo index.html: smooth scrolling, tooltip y mensaje de alerta en seccion de contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,34 @@
-// smooth scrolling
-$(document).ready(function(){
-    $("a").on('click', function(event){
-        if (this.hash !== ""){
-            event.preventDefault();
-            let hash = this.hash;
-            $('html,body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        };
+$(function () {
+  // tooltip
+  $('[data-toggle ="tooltip"]').tooltip({
+    boundary: "window",
+    template:
+      '<div class="tooltip tooltip-custom" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>',
+  });
+
+  // mensaje alerta
+  $(document).ready(function () {
+    $("#enviar").click(function () {
+      alert("El Mensaje fue enviado correctamente...");
     });
+  });
+
+  // smooth scrolling
+  $(document).ready(function () {
+    $("a").on("click", function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        let hash = this.hash;
+        $("html,body").animate(
+          {
+            scrollTop: $(hash).offset().top,
+          },
+          800,
+          function () {
+            window.location.hash = hash;
+          }
+        );
+      }
+    });
+  });
 });
